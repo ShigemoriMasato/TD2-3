5a6a996645937180074ebbdd687548df8a44d2bb
将人 薫森 <k024g0027@g.neec.ac.jp>
2026-03-03 15:21:59 +0900

    リザルトのバグとエンドレスのフロア表記の修正

--- DirectXGame/AppSource/Game/GameScene.cpp
+++ DirectXGame/AppSource/Game/GameScene.cpp
@@ -381,7 +381,7 @@ void GameScene::Initialize() {
 
 	// 最初と最後のカウントをするUI
 	startCountUI_ = std::make_unique<StartCountUI>();
-	startCountUI_->Initialize(fontName, drawData, fontLoader_, commonData_->nextMapIndex, "RampartOne-Regular.ttf");
+	startCountUI_->Initialize(fontName, drawData, fontLoader_, commonData_->stageCount, "RampartOne-Regular.ttf");
 	startCountUI_->isStart_ = true;
 
 	// ミニマップ操作時の時間操作処理
@@ -442,7 +442,11 @@ void GameScene::Initialize() {
 	// 最初に一度だけ更新処理を呼ぶ
 	InGameScene();
 
-	//OreItemStorageNum::currentOreItemNum_ = 1000000;
+	auto logger = getLogger("Result");
+	logger->info("=========================================================");
+	logger->info("-- Stage {} --");
+	logger->info("MaxOreNum: {}\tKillOreNum: {}\tGetOreNum: {}\tMaxGoldNum: {}\tSumGoldNum: {}\n", 
+		commonData_->maxOreNum, commonData_->killOreNum, commonData_->getOreNum, commonData_->maxGoldNum, commonData_->sumGoldNum_);
 }
 
 void GameScene::InitializeOtherScene() {
@@ -1090,15 +1094,14 @@ Vector3 GameScene::GetPlayerInitPosition() {
 }
 
 void GameScene::SaveGameData() {
-
 	int goldNum = OreItemStorageNum::currentOreItemNum_;
 	int oreNum = unitManager_->GetMaxOreCount();
 	int norma = currentMap_.norma;
 	commonData_->normaAndScore_.push_back({ norma, goldNum });
 	commonData_->sumGoldNum_ += goldNum;
 	commonData_->maxGoldNum = std::max(commonData_->maxGoldNum, goldNum);
+	commonData_->maxOreNum = std::max(commonData_->maxOreNum, oreNum);
 	commonData_->killOreNum += commonData_->oreNum - oreNum;
 	commonData_->oreNum = oreNum;
 	commonData_->goldNum = goldNum;
-
 }
